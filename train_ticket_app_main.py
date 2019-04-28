01508cabeb4c7058cfe87c4dce6f3f3001b118a1
TonyYanHeng <18030606135@163.com>
2019-04-28 16:32:05 +0800

    改用selenium实现

--- train_ticket_app_main.py
+++ train_ticket_app_main.py
@@ -10,12 +10,16 @@ import shutil
 from PIL import Image
 import requests
 import json
-import jieba
-from xml.etree import ElementTree
+from selenium.webdriver.firefox import options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium import webdriver
 
+train_ticket_url = "https://www.12306.cn/index/"
 get_img64_url = "https://kyfw.12306.cn/passport/captcha/captcha-image64?login_site=E&module=login&rand=sjrand"
 login_url = "https://kyfw.12306.cn/passport/web/login"
 home_url = "https://kyfw.12306.cn/otn/login/userLogin"
+username = "719492067@qq.com"
+password = "Hyh20180225"
 sub_img_location = {
     '0_0.png': '35,35',
     '0_1.png': '105,35',
@@ -29,10 +33,35 @@ sub_img_location = {
 
 
 def delete_old_images(images_path):
+    """清空images目录下的内容"""
     if os.path.exists(images_path):
         shutil.rmtree(images_path)
     time.sleep(1)
     os.mkdir(images_path)
+
+
+def generate_one_sub_img(source_img_path, x, y):
+    """生成一张子图"""
+    src_img = Image.open(source_img_path)
+    left = 5 + (67 + 5) * x
+    top = 41 + (67 + 5) * y
+    right = left + 67
+    bottom = top + 67
+    sub_img_name = "%s_%s.png" % (y, x)
+    sub_img_path = os.path.join(os.path.dirname(source_img_path), sub_img_name)
+    sub_img = src_img.crop((left, top, right, bottom))
+    sub_img.save(sub_img_path)
+    return sub_img_path
+
+
+def generate_8_sub_images(source_img_path):
+    """生成8个子图"""
+    result = []
+    for y in range(2):
+        for x in range(4):
+            sub_img_path = generate_one_sub_img(source_img_path, x, y)
+            result.append(sub_img_path)
+    return result
 
 
 def get_baidu_ocr_result(img_path):
@@ -225,27 +254,118 @@ class LoginIns(object):
         print("这是首页！")
 
 
+class LoginBySelenium(object):
+    def __init__(self):
+        self.browser = webdriver.Firefox()
+
+    def prepare_for_download_img64(self):
+        self.browser.get(train_ticket_url)
+        time.sleep(1)
+        login_a = self.browser.find_element_by_xpath('/html/body/div[2]/div/div[1]/div/div/ul/li[3]/a[1]')
+        login_a.click()
+        time.sleep(1)
+        account_login_a = self.browser.find_element_by_xpath('/html/body/div[2]/div[2]/ul/li[2]/a')
+        account_login_a.click()
+        time.sleep(1)
+        self.input_username_and_password()
+
+    def input_username_and_password(self):
+        username_input = self.browser.find_element_by_xpath('//*[@id="J-userName"]')
+        username_input.clear()
+        username_input.send_keys(username)
+        password_input = self.browser.find_element_by_xpath('//*[@id="J-password"]')
+        password_input.clear()
+        password_input.send_keys(password)
+        time.sleep(1)
+
+    def download_img64(self, img64_path):
+        print("开始下载验证码图片！")
+        delete_old_images(os.path.dirname(img64_path))
+        self.prepare_for_download_img64()
+        img64_element = self.browser.find_element_by_xpath('//*[@id="J-loginImg"]')
+        img64_url = img64_element.get_attribute("src")
+        urllib.request.urlretrieve(img64_url, img64_path)
+        print("验证码图片下载完成！")
+
+    def find_sub_img_location(self, img64_path):
+        results = []
+        self.download_img64(img64_path)
+        # target_words = get_baidu_ocr_result(img64_path)
+        # if len(target_words) == 0 or target_words == [""]:
+        #     print("未能识别出当前验证码图片中的文字！")
+        #     results = self.find_sub_img_location(img64_path)
+        # else:
+        #     print("当前验证码图片中的目标文字为：%s" % target_words)
+        #     sub_img_paths = generate_8_sub_images(img64_path)
+        #     for cur_sub_image_path in sub_img_paths:
+        #         cur_shitu_results = get_baidu_shitu_result(cur_sub_image_path)
+        #         cur_sub_image_name = os.path.basename(cur_sub_image_path)
+        #         print("子图%s的识图结果为：%s。" % (cur_sub_image_name, cur_shitu_results))
+        #         if len(cur_shitu_results) == 0:
+        #             continue
+        #         for cur_target_word in target_words:
+        #             for cur_shitu_result in cur_shitu_results:
+        #                 if cur_target_word in cur_shitu_result or cur_shitu_result in cur_target_word:
+        #                     results.append(sub_img_location[cur_sub_image_name])
+        #     if len(results) == 0:
+        #         print("没找到对应的目标子图！")
+        #         results = self.find_sub_img_location(img64_path)
+        #     else:
+        #         results = list(set(results))
+        #         print("找到的目标子图位置为：%s。" % results)
+        return results
+
+    def add_randcode_in_html(self, locations):
+        """选择验证码"""
+        html = ""
+        for cur_location in locations:
+            detail_location = cur_location.split(",")
+            top = str(int(detail_location[1]) + 16)
+            left = str(int(detail_location[0]) - 13)
+            style_value = "top: " + top + "px; left: " + left + "px;"
+            html += "<div randCode=\"+\"" + cur_location + "\"+\" class=\"+\"lgcode-active\"+\" style='%s'></div>" % style_value
+        js = "document.getElementById(\"J-passCodeCoin\").innerHTML=\"%s\";" % html
+        print(js)
+        self.browser.execute_script(js)
+
+    def click_login_button(self):
+        # login_button = self.browser.find_element_by_xpath('//*[@id="J-login"]')
+        # ActionChains(self.browser).click(login_button).perform()
+        js = "document.getElementById(\"J-login\").click();"
+        self.browser.execute_script(js)
+
+    def get_current_url(self):
+        return self.browser.current_url
+
+    def refresh_img64(self):
+        refresh_button = self.browser.find_element_by_xpath('/html/body/div[2]/div[2]/div[1]/div[2]/div[3]/div/div[3]')
+        ActionChains(self.browser).click(refresh_button).perform()
+        time.sleep(3)
+
+    def close_browser(self):
+        self.browser.close()
+
+
 if __name__ == '__main__':
     cur_file_path = os.path.realpath(__file__)
     cur_dir_path = os.path.dirname(cur_file_path)
     images_dir_path = os.path.join(cur_dir_path, 'images')
     original_img_path = os.path.join(images_dir_path, 'original.jpg')
     try:
-        loginIns = LoginIns()
-        imgCheckFlag = False
-        loginFlag = False
-        while not loginFlag:
-            sub_img_locations = loginIns.find_locations(original_img_path)
-            imgCheckFlag = loginIns.verify_image(sub_img_locations)
-            if not imgCheckFlag:
-                print("图形验证码验证失败！")
-                continue
-            else:
-                print("图形验证码验证成功！")
-                loginFlag = loginIns.login_home(sub_img_locations)
-        print("登录成功！")
+        cur_url = "https://kyfw.12306.cn/otn/resources/login.html"
+        login_ins = LoginBySelenium()
+        while cur_url == "https://kyfw.12306.cn/otn/resources/login.html":
+            sub_img_locations = login_ins.find_sub_img_location(original_img_path)
+            # login_ins.add_randcode_in_html(sub_img_locations)
+            print("111")
+            login_ins.click_login_button()
+            time.sleep(3)
+            cur_url = login_ins.get_current_url()
+            print("222")
+            login_ins.refresh_img64()
+            print("333")
     except Exception as e:
         print("Exception happened, the detail is: %s!" % e)
     finally:
-        loginIns.close_session()
-        print("2222")
+        login_ins.close_browser()
+        print("444")
